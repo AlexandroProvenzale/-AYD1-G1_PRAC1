18e11e321554245a222c1ecc26ff7da7644d5ccb
PikaGuty <3024021520101@ingenieria.usac.edu.gt>
2023-02-22 20:34:44 -0600

    Creando componente contacto y mostrando todos y favoritos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
 import Menu from "./Components/menu";
+import ListaContactos from './Components/listaContactos';
+import { useState } from 'react';
 
 function App() {
+  const [filtroFav, setNumClics] = useState(false);
+
+  const mostrarTodos = () => {
+    setNumClics(false);
+  }
+
+  const mostrarFavs = () => {
+    setNumClics(true);
+  }
+
   return (
     <div className="App">
-      <Menu></Menu>
-        <div className="Comps">
-            
+      <div className="divMenu">
+        <Menu mtodos={mostrarTodos} mfavorito={mostrarFavs} ></Menu>
+      </div>
+      
+      <div className="Comps">
+      <div className='container-principal'>
+          <div className='card-contactos'>
+            <h3 className='tituloM'>{filtroFav ? "Favoritos" : "Todos los contactos"}</h3>
+            <ListaContactos mostrar={filtroFav} />
+          </div>
         </div>
+      </div>
     </div>
   );
 }
