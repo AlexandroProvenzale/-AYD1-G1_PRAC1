3e4bae455a849de6059a2b0c790a9fbd9b7f4cb9
PikaGuty <3024021520101@ingenieria.usac.edu.gt>
2023-02-23 00:09:02 -0600

    Componente eliminar

--- frontend/src/Components/contacto.js
+++ frontend/src/Components/contacto.js
@@ -3,7 +3,9 @@ import '../styles/contacto.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faStar, faPhone, faAt, faPerson } from "@fortawesome/free-solid-svg-icons";
 import { faTrashCan, faStarHalfStroke } from "@fortawesome/free-regular-svg-icons";
+import { useState, useEffect } from 'react';
 
+import Swal from "sweetalert2";
 
 const i_trash = <FontAwesomeIcon icon={faTrashCan} />
 const i_pencil = <FontAwesomeIcon icon={faPencil} />
@@ -13,9 +15,61 @@ const i_arroba = <FontAwesomeIcon icon={faAt} />
 const i_telefono = <FontAwesomeIcon icon={faPhone} />
 const i_persona = <FontAwesomeIcon icon={faPerson} />
 
-
+function confirmar(){
+    Swal.fire({
+        text: "Está seguro que desea eliminar este contacto?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, deseo eliminarlo!',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (!result.isConfirmed) {
+          return true
+        }else{
+        return false;
+        }
+      })
+      
+}
 
 function Contacto(props){
+    const [idContacto, setIdContacto] = useState({
+        Id:0
+      });
+
+    const handleClick = async (val) => {
+        idContacto.Id = parseInt(val.target.value)
+        console.log(JSON.stringify(idContacto))
+
+        try {
+        const response = await fetch('http://localhost:5000/eliminarContacto', {
+            method: 'DELETE',
+            headers: {
+            'Content-Type':'application/json',
+            'Access-Control-Allow-Origin':'*',
+            },
+            body: JSON.stringify(idContacto),
+        });
+    
+        if (!response.ok) {
+            throw new Error('Data coud not be fetched!')
+        } else {
+            Swal.fire(
+                "Eliminado",
+                "Se ha eliminado el contacto",
+                "success"
+            )
+            const result = await response.json()
+            console.log(JSON.stringify(result, null, 4))
+        }
+        } catch (e) {
+        console.log(e.message)
+        }
+        
+      }
+
     return (
       <div className="card-contacto">
           <img src={require('../imagenes/profile.png')} className="rounded-circle imagen-contacto" />
@@ -28,7 +82,7 @@ function Contacto(props){
               <p className="correo-contacto">{i_arroba} {props.correo}</p>
               <div className='grupo-botones'>
                   <button type="button" className="btn btn-success btnMod">{i_pencil}</button>
-                  <button type="button" className="btn btn-danger btnDel">{i_trash}</button>
+                  <button type="button" className="btn btn-danger btnDel" onClick={e => handleClick(e, "value")} value={props.id}>{i_trash}</button>
               </div>
           </div>
       </div>
